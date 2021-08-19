3e9e9bf11f877a118962aee7d2d331143660b8d9
Pierre7Agus <64596784+Pierre7Agus@users.noreply.github.com>
2021-08-19 05:53:58 -0500

    Proyecto Subido
    Archivos del proyecto alojados en el servidor

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1 align="center">Landing Page</h1>
+
+## Tabla de Contenidos
+
+- [Vista General](#Vista-General)
+- [Tecnologías Usadas](#tecnologias)
+- [Características](#caracteristicas)
+- [Contacto](#contacto)
+- [Recursos](#recursos)
+
+<!-- vista general -->
+## Vista General
+En este proyecto he realizado una Landing Page, como práctica para mejorar en el manejo de las tecnologías necesarias para realizar este proyecto.
+
+![](./img/screen.png)
+
+## Tecnologías Usadas
+
+- CSS3
+- HTML5
+- JavaScript
+
+## Características
+
+Landing Page con diseño resposivo para cualquier tamaño de pantalla.
+
+## Recursos
+
+- [W3Schools]( https://www.w3schools.com )
+- [MDN]( https://developer.mozilla.org/es/ )
+
+## Contacto
+
+- GitHub [ @Pierre7Agus ]( https://github.com/Pierre7Agus )
+- CodePen [ Pierre7Agus ]( https://codepen.io/pierre7agus/pens/public )
+- Twitter [ @agurto_pierre ]( https://twitter.com/@agurto_pierre )
+
---
+++ script.js
@@ -0,0 +1,37 @@
+const dots = document.querySelectorAll('.circle > ul li');
+const elementos = document.querySelectorAll('.features > div');
+//console.log(dots);
+dots[1].addEventListener('click',()=>{
+	dots[1].classList.toggle('active');
+	elementos[1].classList.toggle('opacity');
+
+	dots[0].classList.remove('active');
+	elementos[0].classList.remove('opacity');
+	dots[2].classList.remove('active');
+	elementos[2].classList.remove('opacity');
+
+});
+
+dots[0].addEventListener('click',()=>{
+	dots[0].classList.toggle('active');
+	elementos[0].classList.toggle('opacity');
+	
+	dots[1].classList.remove('active');
+	elementos[1].classList.remove('opacity');
+	dots[2].classList.remove('active');
+	elementos[2].classList.remove('opacity');
+});
+
+dots[2].addEventListener('click',()=>{
+	dots[0].classList.remove('active');
+	dots[1].classList.remove('active');
+	elementos[1].classList.remove('opacity');
+	elementos[0].classList.remove('opacity');
+
+	elementos[2].classList.toggle('opacity');	
+	dots[2].classList.toggle('active');
+});
+const menu = document.querySelector('.hidden');
+document.querySelector('.hamburguer').addEventListener('click',()=>{
+	menu.classList.toggle('izquierda');
+})
